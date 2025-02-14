00a2713ecd5d19498ff9faad802563bafa44a77f
Enehizena Osarugue <nehirugue@gmail.com>
2025-02-14 13:44:35 +0100

    refactor: remove unnecessary newline in delete_book function

--- api/routes/books.py
+++ api/routes/books.py
@@ -72,4 +72,3 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
-
